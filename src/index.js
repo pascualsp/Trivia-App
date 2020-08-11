a2a64bac202b11672966bb56df006baa42d5f9c3
pascualsp <pascuals@uci.edu>
2020-08-11 16:59:40 -0700

    production static directory fix

--- src/index.js
+++ src/index.js
@@ -17,10 +17,10 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopol
     .catch((e) => console.log(e));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/build'));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, '../client/build'))
     });
 }
 
